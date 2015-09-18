d74f8024c9dfdc5400b1371f1ed28a77b50731fd
Torkild U. Resheim <torkildr@gmail.com>
2015-09-18 12:55:19 +0200

    7: Implement login mechanism
    Task-Url: https://github.com/Itema-as/modelshare/issues/issue/7

---
+++ com.statoil.modelshare/src/com/statoil/modelshare/app/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.statoil.modelshare.app.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
--- com.statoil.modelshare/src/com/statoil/modelshare/app/config/ThymeleafConfig.java
+++ com.statoil.modelshare/src/com/statoil/modelshare/app/config/ThymeleafConfig.java
@@ -1,9 +1,14 @@
 package com.statoil.modelshare.app.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -12,21 +17,23 @@ import org.thymeleaf.templateresolver.TemplateResolver;
 @Configuration
 @PropertySource("classpath:thymeleaf.properties")
 public class ThymeleafConfig {
-
-
+	
     @Bean
     public TemplateResolver templateResolver(){
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("HTML5");
-
         return templateResolver;
     }
 
     @Bean
     public SpringTemplateEngine templateEngine(){
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        Set<IDialect> dialects = new HashSet<IDialect>();
+        dialects.add(springSecurityDialect());
+
+        templateEngine.setAdditionalDialects(dialects);  
         templateEngine.setTemplateResolver(templateResolver());
         return templateEngine;
     }
@@ -36,20 +43,12 @@ public class ThymeleafConfig {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver() ;
         viewResolver.setTemplateEngine(templateEngine());
         viewResolver.setOrder(1);
-
         return viewResolver;
     }
-
-    /**
-     //Example for JSP
-     @Bean
-     public ViewResolver viewResolver() {
-         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-         viewResolver.setViewClass(JstlView.class);
-         viewResolver.setPrefix("/WEB-INF/pages/");
-         viewResolver.setSuffix(".jsp");
-
-         return viewResolver;
-     }
-     **/
+    
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        SpringSecurityDialect dialect = new SpringSecurityDialect();
+        return dialect;
+    }
 }
