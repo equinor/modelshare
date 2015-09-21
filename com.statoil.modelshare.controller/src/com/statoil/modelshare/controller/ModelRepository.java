0910e01f55603567d583febfcb7cf2d75c4e06dc
rhe <rhe@itema.no>
2015-09-21 14:54:50 +0200

    API for uploading, creating folder and deleting folder

--- com.statoil.modelshare.controller/src/com/statoil/modelshare/controller/ModelRepository.java
+++ com.statoil.modelshare.controller/src/com/statoil/modelshare/controller/ModelRepository.java
@@ -1,5 +1,6 @@
 package com.statoil.modelshare.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
@@ -51,4 +52,19 @@ public interface ModelRepository {
 	 * @return
 	 */
 	public boolean hasDisplayAccess(User user, Path path) throws IOException ;
+	
+	/**
+	 * Uploads a file based on information given in the view
+	 */
+	public void uploadFile(Folder folder, File file, String owner, String organisation, String usage);
+	
+	/**
+	 * Creates a folder on the given parent folder and the name of the new folder
+	 */
+	public void createFolder(Folder parentFolder, String name);
+	
+	/**
+	 * Delete a folder given the parent folder
+	 */
+	public void deleteFolder(Folder parentFolder, Folder folder);
 }
