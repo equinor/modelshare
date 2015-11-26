75b6097d8f3541bf5353ff4eac6e2ce3c69678e6
Torkild U. Resheim <torkildr@gmail.com>
2015-11-26 13:15:54 +0100

    159: Image streams are not properly closed
    Task-Url: https://github.com/Itema-as/modelshare/issues/issue/159

--- com.statoil.modelshare/src/com/statoil/modelshare/app/web/ImageController.java
+++ com.statoil.modelshare/src/com/statoil/modelshare/app/web/ImageController.java
@@ -13,6 +13,7 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.TikaMetadataKeys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -38,14 +39,14 @@ public class ImageController {
 
 	@RequestMapping(value = "/pictures", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<InputStreamResource> picture(@RequestParam(value = "asset", required = true) String asset,
+	public ResponseEntity<FileSystemResource> picture(@RequestParam(value = "asset", required = true) String asset,
 			Principal principal) {
 		Client user = repository.getUser(principal.getName());
 		try {
 			File file = repository.getFile(user, Paths.get(asset));
 			if (file.exists() && file.isFile()) {
-				return ResponseEntity.ok().contentLength(file.length()).contentType(getMimeType(file))
-						.body(new InputStreamResource(new BufferedInputStream(Files.newInputStream(file.toPath(), StandardOpenOption.READ))));
+				return ResponseEntity.ok().contentLength(file.length()).contentType(MediaType.IMAGE_JPEG)
+						.body(new FileSystemResource(file));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -53,20 +54,20 @@ public class ImageController {
 		return null;
 	}
 
-	private MediaType getMimeType(File file) {
-		try {
-			if (tika == null) {
-				tika = new TikaConfig();
-			}
-			Metadata metadata = new Metadata();
-			metadata.set(TikaMetadataKeys.RESOURCE_NAME_KEY, file.getName());
-			BufferedInputStream bis = new BufferedInputStream(
-					Files.newInputStream(file.toPath(), StandardOpenOption.READ));
-			return MediaType.parseMediaType(tika.getDetector().detect(bis, metadata).toString());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (TikaException e) {
-			throw new RuntimeException(e);
-		}
-	}
+//	private MediaType getMimeType(File file) {
+//		try {
+//			if (tika == null) {
+//				tika = new TikaConfig();
+//			}
+//			Metadata metadata = new Metadata();
+//			metadata.set(TikaMetadataKeys.RESOURCE_NAME_KEY, file.getName());
+//			BufferedInputStream bis = new BufferedInputStream(
+//					Files.newInputStream(file.toPath(), StandardOpenOption.READ));
+//			return MediaType.parseMediaType(tika.getDetector().detect(bis, metadata).toString());
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		} catch (TikaException e) {
+//			throw new RuntimeException(e);
+//		}
+//	}
 }
